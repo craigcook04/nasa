87382cd172d245b8a490d971cd9c77ab35e96c01
Craig Cook <ccook87@uwo.ca>
2018-02-17 00:14:22 +0000

    Show all NEO's on start day

--- NasaApp/app.js
+++ NasaApp/app.js
@@ -32,8 +32,6 @@ app.get('/show', function(req, res) {
     request('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + startDate + '&end_date=' + endDate + '&api_key=WgUOHmvbWyaPR8rGmCwD6Kii3u732VutWXa75GWb', function(error, response, body) {
         if(!error && response.statusCode == 200) { // 200 OK status code
             var parsedData = JSON.parse(body);
-            console.log(parsedData['near_earth_objects']);
-        
             res.render('show', {
                 body: parsedData,
                 startDate: startDate,
